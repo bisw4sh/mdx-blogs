d5b0330ebacde2429c3f8b72e19dab2ca3939bef
Biswash Dhungana <spikeystona@gmail.com>
2024-07-18 17:02:56 +0545

    refactor: small deduction of dead code

--- backend/controllers/save-mdx.ts
+++ backend/controllers/save-mdx.ts
@@ -6,13 +6,9 @@ import path from "path";
 export const save_mdx = async (req: Request, res: Response) => {
   try {
     const { markdown, filename } = req.body;
-    console.log(markdown, filename);
 
-    const fileOperation = await writeFile(
-      path.join(__dirname, "files", `${filename}.mdx`),
-      markdown
-    );
-    console.log(fileOperation);
+    await writeFile(path.join(__dirname, "files", `${filename}.mdx`), markdown);
+
     res.status(201).json({ message: "successfully created" });
   } catch (error) {
     console.error(error);
