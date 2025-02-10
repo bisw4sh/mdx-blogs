1991e096acc608faa1cf20d9076b54b8f23d6976
bisw4sh <spikeystona@gmail.com>
2025-02-11 00:50:24 +0545

    fix: the disfigured mode toggle icons

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -36,7 +36,7 @@ export default function ThemeToggle({ mode, setMode }: PropTypes) {
 
   return (
     <>
-      <button type="button" className="absolute top-4 right-4">
+      <button className="absolute top-4 right-4" type="button">
         <label className="flex cursor-pointer gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -49,9 +49,10 @@ export default function ThemeToggle({ mode, setMode }: PropTypes) {
             strokeLinecap="round"
             strokeLinejoin="round"
           >
-            <title>Light Mode Icon</title>
+            <title>Light mode icon</title>
             <circle cx="12" cy="12" r="5" />
             <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+          </svg>
           <input
             type="checkbox"
             value="synthwave"
@@ -67,10 +68,10 @@ export default function ThemeToggle({ mode, setMode }: PropTypes) {
             stroke="currentColor"
             strokeWidth="2"
             strokeLinecap="round"
+            strokeLinejoin="round"
           >
-            <title>Dark Mode Icon</title>
+            <title>Dark mode icon</title>
             <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
-          </svg>
           </svg>
         </label>
       </button>
